214e90bf564fc57f8914e039c6022c082491501c
abhi12-ayalur <abhinav.ayalur@gmail.com>
2022-10-25 06:42:18 +0000

    refactor

--- predictor.py
+++ predictor.py
@@ -1,15 +1,15 @@
 from typing import List
 from unittest import result
-from sieve.types import FrameSingleObject, UserMetadata, SingleObject, BoundingBox, TemporalObject
-from sieve.predictors import TemporalProcessor
+from sieve.types import FrameSingleObject, UserMetadata, SingleObject, BoundingBox, Temporal
+from sieve.predictors import TemporalPredictor
 from hyperlpr import HyperLPR_plate_recognition
 
 
-class PlateExtractor(TemporalProcessor):
+class PlateExtractor(TemporalPredictor):
     def predict(self, frame: FrameSingleObject, metadata: UserMetadata) -> List[FrameSingleObject]:
         output_objects = []
-        frame_number = frame.temporal_object.frame_number
-        frame_data = frame.temporal_object.get_array()
+        frame_number = frame.get_temporal().frame_number
+        frame_data = frame.get_temporal().get_array()
         try: 
             results = HyperLPR_plate_recognition(frame_data) 
         except Exception as e:
@@ -27,6 +27,6 @@ class PlateExtractor(TemporalProcessor):
     def postprocess(self, plate, conf, bbox, frame_number):
         out = []
         bounding_box = BoundingBox.from_array([float(i) for i in bbox])
-        temporal_object = TemporalObject(frame_number=frame_number, bounding_box=bounding_box, score=conf, plate=plate)
-        out.append(SingleObject(cls="license-plate", temporal_object=temporal_object))
+        temporal = Temporal(frame_number=frame_number, bounding_box=bounding_box, score=conf, plate=plate)
+        out.append(SingleObject(cls="license-plate", temporal=temporal))
         return out
